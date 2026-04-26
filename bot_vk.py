e0c82690c8518460a456ea7a094f289741aac402
Andrey9045 <andreidikun123@gmail.com>
2026-04-26 22:55:04 +0300

    Update bot_vk.py

--- bot_vk.py
+++ bot_vk.py
@@ -9,7 +9,7 @@ from google.cloud import dialogflow
 def detect_intent_texts(project_id, event, vk_api):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, event.user_id)
-    text_input = dialogflow.TextInput(text = event.text, language_code='ru')
+    text_input = dialogflow.TextInput(text=event.text, language_code='ru')
     query_input = dialogflow.QueryInput(text=text_input)
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
@@ -19,15 +19,15 @@ def detect_intent_texts(project_id, event, vk_api):
             user_id=event.user_id,
             message=response.query_result.fulfillment_text,
             random_id=random.randint(1, 1000)
-            )
+        )
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     load_dotenv()
-    vk_token=os.getenv("VK_TOKEN")
+    vk_token = os.getenv("VK_TOKEN")
     project_id = 'exalted-ability-494118-b0'
     vk_session = vk_api.VkApi(token=vk_token)
-    vk_api = vk_session.get_api() 
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
